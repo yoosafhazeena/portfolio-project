23d4f9b045ab83a3502f16aa8a2007fa6ed42e84
Hazeena Yoosaf <114492454+yoosafhazeena@users.noreply.github.com>
2022-10-25 16:11:11 +0530

    Initial commit

---
+++ blog/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from blog.models import Post, Comment
+
+# Create your views here.
+def blog_index(request):
+    posts = Post.objects.all().order_by('-created_on')
+    context = {
+        "posts" : posts
+    }
+    return render(request, "blog_index.html", context)
