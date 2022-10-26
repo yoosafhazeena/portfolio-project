0c4ddc07ab0c4b7c5cd312c25da0e0f700c67931
Hazeena Yoosaf <114492454+yoosafhazeena@users.noreply.github.com>
2022-10-26 12:35:55 +0530

    static warning corrected in settings

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -54,9 +55,7 @@ MIDDLEWARE = [
 #added manually for static files
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'projects\static'), )
 
 ROOT_URLCONF = 'portfolio.urls'
 
